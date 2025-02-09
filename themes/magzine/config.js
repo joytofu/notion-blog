665cc0d6384d77521c3f9520a73427f878eb0fe3
joytofu <kcswag@foxmail.com>
2025-02-09 15:47:13 +0800

    update

--- themes/magzine/config.js
+++ themes/magzine/config.js
@@ -3,20 +3,19 @@ const CONFIG = {
   MAGZINE_HOME_BANNER_ENABLE: true, // 首屏右上角的宣传位
   MAGZINE_HOME_BUTTON: true,
   MAGZINE_HOME_BUTTON_URL: '/about',
-  MAGZINE_HOME_BUTTON_TEXT: '了解更多',
+  MAGZINE_HOME_BUTTON_TEXT: 'Dig Deeper',
 
   MAGZINE_HOME_HIDDEN_CATEGORY: '分享杂文', //不希望在首页展示的文章分类，用英文逗号隔开
 
-  MAGZINE_HOME_TITLE: '立即开创您的在线业务。完全免费。',
+  MAGZINE_HOME_TITLE: 'Learning Long Life',
   MAGZINE_HOME_DESCRIPTION:
-    '借助NotionNext，获得助您开创、经营和扩展业务所需的全部工具和帮助。',
-  MAGZINE_HOME_TIPS: 'AI时代来临，这是属于超级个体的狂欢盛宴！',
+    'Mastering the Art of Self-Improvement: Your Guide to Lifelong Learning',
 
   // 首页底部推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
-  MAGZINE_RECOMMEND_POST_TAG: '推荐',
+  MAGZINE_RECOMMEND_POST_TAG: '',
   MAGZINE_RECOMMEND_POST_COUNT: 6,
-  MAGZINE_RECOMMEND_POST_TITLE: '推荐文章',
-  MAGZINE_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
+  MAGZINE_RECOMMEND_POST_TITLE: 'Featured',
+  MAGZINE_RECOMMEND_POST_SORT_BY_UPDATE_TIME: true, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
 
   // Style
   MAGZINE_RIGHT_PANEL_DARK: process.env.NEXT_PUBLIC_MAGZINE_RIGHT_DARK || false, // 右侧面板深色模式
@@ -27,7 +26,7 @@ const CONFIG = {
   MAGZINE_POST_LIST_TAG: true, // 列表显示文章标签
 
   MAGZINE_POST_DETAIL_CATEGORY: true, // 文章显示分类
-  MAGZINE_POST_DETAIL_TAG: true, // 文章显示标签
+  MAGZINE_POST_DETAIL_TAG: false, // 文章显示标签
 
   // 文章页面联系卡
   MAGZINE_SOCIAL_CARD: false, // 是否显示右侧，点击加入社群按钮
@@ -38,7 +37,7 @@ const CONFIG = {
 
   // 页脚菜单
   MAGZINE_FOOTER_LINKS: [
-    {
+    /* {
       name: '友情链接',
       menus: [
         {
@@ -101,7 +100,7 @@ const CONFIG = {
         { title: '建站工具', href: 'https://www.tangly1024.com/' },
         { title: 'NotionNext', href: 'https://docs.tangly1024.com/about' }
       ]
-    }
+    } */
   ],
 
   // 旧版本顶部菜单
