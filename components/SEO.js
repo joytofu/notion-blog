05f7da1112de3060e92f387f3c0a76e1f5653ca0
joytofu <kcswag@foxmail.com>
2025-09-17 21:25:06 +0800

    add new google tag

--- components/SEO.js
+++ components/SEO.js
@@ -210,7 +210,22 @@ const SEO = props => {
         </>
       )}
 
-      
+      {
+        <>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-E3ZVG4PJHP"></script>
+          <script
+            dangerouslySetInnerHTML={{
+            __html: `
+                  window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-E3ZVG4PJHP');
+                                    `
+                                  }}
+          />
+        </>
+      }
+
       {children}
       
     </Head>
