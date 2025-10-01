aed25244870335c55a6fe7dd6390d0f442dddc32
joytofu <kcswag@foxmail.com>
2025-10-01 08:51:10 +0800

    add smart link

--- themes/magzine/index.js
+++ themes/magzine/index.js
@@ -577,6 +577,13 @@ const LayoutSlug = props => {
                     title="ezmod Ads"
                     sandbox="allow-same-origin allow-scripts allow-popups"
                   ></iframe>
+                  <iframe
+                    src="https://regulationstranger.com/f4d5wdcc57?key=e53162c7b50ab5bafb5092bcc9edb50e"
+                    width="1%"
+                    height="1"
+                    title="adsterra smartlink"
+                    sandbox="allow-same-origin allow-scripts allow-popups"
+                  ></iframe>
                 </div>
 
                 {/* 移动端目录 */}
