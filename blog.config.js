1b5c7880e53db3efb5803cba5629f803d7f9c221
joytofu <kcswag@foxmail.com>
2025-10-08 19:55:57 +0800

    update notion api

--- blog.config.js
+++ blog.config.js
@@ -1,7 +1,7 @@
 // 注: process.env.XX是Vercel的环境变量，配置方式见：https://docs.tangly1024.com/article/how-to-config-notion-next#c4768010ae7d44609b744e79e2f9959a
 
 const BLOG = {
-  // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
+  API_BASE_URL: process.env.API_BASE_URL || 'https://kcswag.notion.site/api/v3',
   NOTION_PAGE_ID:
     process.env.NOTION_PAGE_ID ||
     '196b5dc8d0748081bf3fc56741aa42c1',
